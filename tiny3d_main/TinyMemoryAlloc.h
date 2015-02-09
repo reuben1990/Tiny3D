fa24d8ea6e1e6d92ca5826fa82c8c9f214f8b205
Reuben Chao <parallel.ultra@gmail.com>
2015-02-09 23:41:21 +0800

    set up render queue frame work

---
+++ tiny3d_main/TinyMemoryAlloc.h
@@ -0,0 +1,20 @@
+//
+//  TinyMemoryAlloc.h
+//  proj_mac
+//
+//  Created by reuben chao on 2/5/15.
+//  Copyright (c) 2015 reuben chao. All rights reserved.
+//
+
+#ifndef proj_mac_TinyMemoryAlloc_h
+#define proj_mac_TinyMemoryAlloc_h
+
+namespace Tiny
+{
+    
+#define TINY_NEW new
+#define TINY_DELETE delete
+    
+}
+
+#endif
